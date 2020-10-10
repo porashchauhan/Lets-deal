d5db99d4e2dbfc131253e46f3c228c513ec9dc66
harshil1509 <singhalharshil96@gmail.com>
2020-10-10 23:54:23 +0530

    added about

---
+++ about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="mobiles, watchs shopping, handsets, smartwatches">
+        <meta name="Author" content="Vishal Kumar">
+        <title>Welcome | BookMyKart</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+    </head>
+    <body style="padding-top: 50px;">
+	  <div class="container-fluid">
+          <?php include 'includes/header.php'; ?>
+        <div class="row" style="margin-top: 70px;">
+            <div class="container text-justify">
+                    <div class="col-md-4">
+                        <div class="panel">
+                            <div class="panel-heading">
+                               <div class="panel-title">
+							    <h4 style="color: grey;">WHO WE ARE</h4>
+							   </div>
+							</div>
+                            <div class="thumbnail">
+                                <img src="img/cart.jpg" alt="about">
+                            </div><br>
+                            <div class="panel-body">
+							 <p style="font-weight: normal; font-size: 1.2em;">
+							      Lets_deal is an online second hand goods selling E-commerce website currently operating in IIT Bhilai, Its basically designed under the motto "for the students by the studnts". Here any student of IIT bhilai can sell his/her used or unused item at a resonable price so that after him/her other students can take benefit of that item.
+							 </p>
+                            </div>
+                        </div>
+                    </div>
+                     <div class="col-md-4">
+                        <div class="panel">
+                            <div class="panel-heading">
+							    <div class="panel-title">
+                                   <h4 style="color: grey;">OUR HISTORY</h4>
+								</div>
+                            </div>
+							
+                </div>
+            </div>
+		</div>
+         <?php
+        include 'includes/footer.php';
+        ?>
+
+		<?php include 'modal.php' ?>
+    </body>
+</html>
